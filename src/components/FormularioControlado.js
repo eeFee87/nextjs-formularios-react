746b1a4ca75b7ac45f9b4946c0946fa15ed9f4b9
efe <eeFee1987@gmail.com>
2024-02-15 07:39:18 +0100

    create page controlled forms

--- src/components/FormularioControlado.js
+++ src/components/FormularioControlado.js
@@ -1,3 +1,25 @@
-export function FormularioControlado() {
-    return <div>FormularioControlado</div>
+'use client'
+import { useState } from "react";
+
+export function FormularioControlado(props) {
+  const [emailAddress, setEmailAddress] = useState('');
+
+  const handleChange = (e) => {
+    setEmailAddress(e.target.value);
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="email">Introduce tu dirección de email:</label>
+        <input
+          value={emailAddress}
+          onChange={handleChange}
+          type="text"
+          id="email"
+        />
+      </form>
+      <p>Tu dirección de email es: {emailAddress}</p>
+    </>
+  );
 }
